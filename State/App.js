920ba3b063376bec8fbe27dec7fcd77a5ce0f88c
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-07-11 12:17:09 -0300

    Demonstração de State no react-native. É igual aos states do react.

---
+++ State/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+class BlinkingText extends Component{
+    //construtor, setando o estado inicial do objeto
+    constructor(props){
+        super(props);
+        this.state = {isShowingText:true};
+        setInterval(()=>{
+            this.setState(previousState=>{
+                return {isShowingText:!previousState.isShowingText};
+            });
+        }, 1000);
+    }
+
+    render(){
+        let textToShow = this.state.isShowingText ? this.props.text : '';
+        return (
+            <Text>{textToShow}</Text>
+        );
+    }
+}
+
+export default class App extends Component{
+    render(){
+        return(
+            <View>
+                <BlinkingText text='CHARLES BRONSON'/>
+            </View>
+        )
+    }
+}
